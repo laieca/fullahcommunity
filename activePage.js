8f3d2cdbb5e49fcc0b61b9108454107fbbdf55c8
laieca <laiecaleeds@gmail.com>
2024-05-27 02:16:35 +0100

    Delete activePage.js

--- activePage.js
+++
@@ -1,7 +0,0 @@
-const  activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('.topnav a').forEach(link =>{
-    if(link.href.includes(`${activePage}`)){
-        link.classList.add('active');
-    }
-
-})
